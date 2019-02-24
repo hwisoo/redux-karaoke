da98df7bcac3c57b6d7c2991401e4e3386b69045
James Cho <hwisoocho@gmail.com>
2019-02-23 17:04:23 -0800

    Multiple Redux Action Types

--- scripts.js
+++ scripts.js
@@ -73,12 +73,13 @@ window.onload = function() {
 
 // CLICK LISTENER
 const userClick = () => {
-  const currentState = store.getState();
-  if (currentState.arrayPosition === currentState.songLyricsArray.length - 1) {
+  if (
+    store.getState().arrayPosition ===
+    store.getState().songLyricsArray.length - 1
+  ) {
     store.dispatch({ type: "RESTART_SONG" });
   } else {
     store.dispatch({ type: "NEXT_LYRIC" });
-    console.log(store.getState());
   }
 };
 
