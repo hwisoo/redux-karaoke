b0823be63dfa2d7624ada93fbb87ba859dd83abf
James Cho <hwisoocho@gmail.com>
2019-03-03 12:29:12 -0800

    set components

---
+++ src/components/SongSearch.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+function SongSearch() {
+  let input;
+  return (
+    <div>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          if (!input.value.trim()) {
+            return;
+          }
+          // ...we'll add more custom logic here later...
+          console.log("SEARCHED TITLE:");
+          console.log(input.value.trim());
+          //...instead of these console.log()s....
+          input.value = "";
+        }}
+      >
+        <input
+          placeholder="Song Title"
+          ref={node => {
+            input = node;
+          }}
+        />
+        <button>Search</button>
+      </form>
+    </div>
+  );
+}
+
+export default SongSearch;
